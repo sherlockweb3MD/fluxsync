dd32054ffca421d54e43aa011b294af3ca6937f3
victorblvck <victorigbokwe60@gmail.com>
2025-04-11 13:57:56 +0100

    small correction

--- components/home-page/intro.tsx
+++ components/home-page/intro.tsx
@@ -3,8 +3,7 @@ import { Twemoji } from '~/components/ui/twemoji'
 export function Intro() {
   return (
     <h1 className="text-neutral-900 dark:text-neutral-200">
-      I'm <span className="font-medium">Victor Igbokwe</span> - a passionate Software Engineer in
-      <span className="hidden font-medium">Nigeria</span>
+      I'm <span className="font-medium">Victor Igbokwe</span> - a passionate Blogger
       <span className="absolute ml-1.5 inline-flex pt-[3px]">
         <Twemoji emoji="flag-nigeria" />
       </span>
